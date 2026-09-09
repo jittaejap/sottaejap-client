058f47566c347a0e1f0b013100e3bb57df96b7b5
govlfox <92joker@naver.com>
2026-09-08 23:38:31 -0700

    test(ui): 정본 로그인과 하단 탭 구성을 고정 (#21)

---
+++ src/__tests__/appBottomNav.spec.ts
@@ -0,0 +1,25 @@
+import { mount } from '@vue/test-utils'
+import { createMemoryHistory, createRouter } from 'vue-router'
+import { describe, expect, it } from 'vitest'
+
+import AppBottomNav from '@/components/common/AppBottomNav.vue'
+import { routes } from '@/router'
+
+describe('하단 탭', () => {
+  it('IA 3-1~3-4의 네 탭 이름과 경로를 사용한다', async () => {
+    const router = createRouter({ history: createMemoryHistory(), routes: [...routes] })
+    await router.push('/')
+    await router.isReady()
+
+    const wrapper = mount(AppBottomNav, { global: { plugins: [router] } })
+    const links = wrapper.findAll('a')
+
+    expect(links.map((link) => link.text())).toEqual(['홈', 'AI채팅', '지도', '거래내역'])
+    expect(links.map((link) => link.attributes('href'))).toEqual([
+      '/',
+      '/chat',
+      '/map',
+      '/transactions',
+    ])
+  })
+})
